e5fa5c759943da6e84bbfaa2f45d2cb915f2892e
Hernan Rajchert <hrajchert@gmail.com>
2019-04-25 18:31:40 -0300

    PS Added sortBy to the compiled output

--- src/Main.purs
+++ src/Main.purs
@@ -5,8 +5,10 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 import QSort (cmpNumberAsc, mutableQSortBy, qsortBy)
+import Data.Array (sortBy)
 
 main :: Effect Unit
 main = do
   log $ show $ mutableQSortBy cmpNumberAsc [1, 2, 3]
   log $ show $ qsortBy cmpNumberAsc [1, 2, 3]
+  log $ show $ sortBy cmpNumberAsc [1, 2, 3]
